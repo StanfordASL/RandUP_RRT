b74cfb367396f6cd04b9de78595868cb25c75946
Albert Wu <amhwu@stanford.edu>
2022-03-17 09:36:10 -0700

    deprecated python <3.7 support

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 from setuptools import setup, find_packages
 import sys
 
-REQUIRES=[
-    "numpy",
-    "matplotlib",
-    "rtree",
-    "pybullet",
-    "scipy"
-]
-if sys.version_info < (3, 7):
-    REQUIRES.append("dataclasses==0.8")
-
 setup(
     name="randUP_RRT",
     version="0.0.1",
